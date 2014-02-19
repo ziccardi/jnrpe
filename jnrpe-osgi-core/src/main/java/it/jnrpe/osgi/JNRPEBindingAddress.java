8b50e32805cf01b805058e56488c3533f48be406
ziccardi <massimiliano.ziccardi@gmail.com>
2014-02-19 17:59:42 +0100

    Added to the repository

---
+++ jnrpe-osgi-core/src/main/java/it/jnrpe/osgi/JNRPEBindingAddress.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2013 Massimiliano Ziccardi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.jnrpe.osgi;
+
+class JNRPEBindingAddress {
+
+    private boolean ssl = false;
+    private String address;
+    private int port = 5667;
+
+    JNRPEBindingAddress(final String bindingAddress) {
+        init(bindingAddress);
+    }
+
+    private void init(final String bindingAddress) {
+
+        String addr = bindingAddress;
+
+        ssl = addr.startsWith("SSL/");
+        if (ssl) {
+            addr = addr.substring(4);
+        }
+
+        if (addr.indexOf(':') != -1) {
+            String[] components = addr.split(":");
+            address = components[0];
+            port = Integer.parseInt(components[1]);
+        } else {
+            address = addr;
+        }
+    }
+
+    protected boolean isSsl() {
+        return ssl;
+    }
+
+    protected String getAddress() {
+        return address;
+    }
+
+    protected int getPort() {
+        return port;
+    }
+}
---
+++ jnrpe-osgi-core/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2012 Massimiliano Ziccardi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.slf4j.impl;
+
+import org.slf4j.ILoggerFactory;
+
+public class StaticLoggerBinder
+{
+    private static OSGILogFactory s_factory = new OSGILogFactory();
+
+    /**
+     * The unique instance of this class.
+     * 
+     * @deprecated Please use the {@link #getSingleton()} method instead of
+     *             accessing this field directly. In future versions, this field
+     *             will become private.
+     */
+    @Deprecated
+    public static final StaticLoggerBinder SINGLETON = new StaticLoggerBinder();
+
+    /**
+     * Return the singleton of this class.
+     * 
+     * @return the StaticLoggerBinder singleton
+     */
+    public static final StaticLoggerBinder getSingleton() {
+        return SINGLETON;
+    }
+
+    /**
+     * Declare the version of the SLF4J API this implementation is compiled against.
+     * The value of this field is usually modified with each release.
+     */
+    // to avoid constant folding by the compiler, this field must *not* be final
+    public static String REQUESTED_API_VERSION = "1.7.5";  // !final
+
+    private StaticLoggerBinder()
+    {
+    }
+
+    public ILoggerFactory getLoggerFactory()
+    {
+        return s_factory;
+    }
+
+    public String getLoggerFactoryClassStr() {
+        return null;
+    }
+
+}
