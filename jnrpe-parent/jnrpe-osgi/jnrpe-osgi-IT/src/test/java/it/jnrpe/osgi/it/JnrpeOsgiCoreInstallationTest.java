55c205d95983726a8a28bc188f700eb85175bc84
ziccardi <massimiliano.ziccardi@gmail.com>
2014-03-21 11:23:49 +0100

    Fixed copyright Fixed tracker address

--- jnrpe-parent/jnrpe-osgi/jnrpe-osgi-IT/src/test/java/it/jnrpe/osgi/it/JnrpeOsgiCoreInstallationTest.java
+++ jnrpe-parent/jnrpe-osgi/jnrpe-osgi-IT/src/test/java/it/jnrpe/osgi/it/JnrpeOsgiCoreInstallationTest.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2014 Massimiliano Ziccardi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package it.jnrpe.osgi.it;
 
 import static org.ops4j.pax.exam.CoreOptions.junitBundles;
@@ -229,7 +244,7 @@ public class JnrpeOsgiCoreInstallationTest {
 	}
 
 	/**
-	 * This test checksthat the core bundle is active.
+	 * This test checks that the core bundle is active.
 	 */
 	@Test
 	public void checkJnrpeOsgiCoreBundle() {
