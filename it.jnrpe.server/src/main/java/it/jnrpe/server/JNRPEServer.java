fff27d04a26d93ea9c43711dbd91bba4b5cbd90b
Massimiliano Ziccardi <mziccard@redhat.com>
2020-09-11 17:34:40 +0200

    feat: 🎸 added config parameter to the main class
    Now when running JNRPE a -c|--conf parameter can be specified with the
    path of the configuration file. If not specified, /etc/jnrpe/jnrpe.yml
    is used as default

--- it.jnrpe.server/src/main/java/it/jnrpe/server/JNRPEServer.java
+++ it.jnrpe.server/src/main/java/it/jnrpe/server/JNRPEServer.java
@@ -20,13 +20,23 @@ import it.jnrpe.engine.services.config.Binding;
 import it.jnrpe.engine.services.config.IConfigProvider;
 import it.jnrpe.engine.services.config.JNRPEConfig;
 import it.jnrpe.engine.services.network.INetworkListener;
+import it.jnrpe.engine.services.plugins.CommandLine.Command;
+import it.jnrpe.engine.services.plugins.CommandLine.Option;
 import java.io.File;
 import java.util.Optional;
 import java.util.ServiceLoader;
 
+@Command(name = "jnrpe")
 public class JNRPEServer {
+  @Option(
+      names = {"-c", "--conf"},
+      defaultValue = "/etc/jnrpe/jnrpe.yml",
+      paramLabel = "PATH",
+      required = true,
+      description = "Path to the JNRPE server config file (defaults to '/etc/jnrpe/jnrpe.yml')")
+  private String confFile;
 
-  private static void bind(Binding binding) {
+  private void bind(Binding binding) {
     // retrieve the network service
     ServiceLoader<INetworkListener> serviceLoader = ServiceLoader.load(INetworkListener.class);
     Optional<INetworkListener> listener = serviceLoader.findFirst();
@@ -44,9 +54,15 @@ public class JNRPEServer {
     }
   }
 
-  public static void main(String[] args) {
+  public void start() throws Exception {
     // Parsing the configuration
-    File confFile = new File("/Users/ziccardi/Desktop/conf.yaml");
+    File confFile = new File(this.confFile);
+    if (!confFile.canRead()) {
+      EventManager.fatal("Unable to read the configuration file at %s", confFile.getAbsolutePath());
+      //System.exit(-1);
+      throw new Exception(String.format("Unable to read the configuration file at %s", confFile.getAbsolutePath()));
+    }
+
     ConfigSource.setConfigFile(confFile);
 
     ServiceLoader<IConfigProvider> configProviderServiceLoader =
@@ -63,7 +79,7 @@ public class JNRPEServer {
     }
 
     config.ifPresentOrElse(
-        cfg -> cfg.getServer().getBindings().forEach(JNRPEServer::bind),
+        cfg -> cfg.getServer().getBindings().forEach(this::bind),
         () ->
             EventManager.fatal(
                 "No config provider has been able to parse the provided config file (%s)",
