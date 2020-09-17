52c6527f84375a8134cfbb5dcb917b58181b6509
Massimiliano Ziccardi <mziccard@redhat.com>
2020-09-17 11:07:05 +0200

    feat(plugins): implemented plugin execution
    This is just the first draft

--- it.jnrpe.services.plugins/src/main/java/module-info.java
+++ it.jnrpe.services.plugins/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 import it.jnrpe.engine.services.plugins.IPlugin;
 import it.jnrpe.services.plugins.CheckDiskPlugin;
+import it.jnrpe.services.plugins.CheckTestPlugin;
 
 module jnrpe.it.jnrpe.services.plugins {
   requires it.jnrpe.engine;
@@ -8,5 +9,6 @@ module jnrpe.it.jnrpe.services.plugins {
       it.jnrpe.engine;
 
   provides IPlugin with
-      CheckDiskPlugin;
+      CheckDiskPlugin,
+      CheckTestPlugin;
 }
