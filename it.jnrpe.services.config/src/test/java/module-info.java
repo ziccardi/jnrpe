84d43eaad017155a3fd67bc72986e33c88484153
Massimiliano Ziccardi <mziccard@redhat.com>
2022-02-19 23:51:59 +0100

    test: added config validator and tests

--- it.jnrpe.services.config/src/test/java/module-info.java
+++ it.jnrpe.services.config/src/test/java/module-info.java
@@ -2,9 +2,11 @@ import it.jnrpe.engine.services.config.IConfigProvider;
 import it.jnrpe.engine.services.config.IConfigSource;
 import it.jnrpe.services.config.yaml.tests.YamlConfigSource;
 
-open module jnrpe.it.jnrpe.services.config.test {
+open module it.jnrpe.services.config.test {
   requires it.jnrpe.engine;
   requires org.junit.jupiter.api;
+  requires it.jnrpe.services.config;
+  requires org.yaml.snakeyaml;
 
   provides IConfigSource with
       YamlConfigSource;
